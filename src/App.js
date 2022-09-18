efc4b5cfc57ccc6bb20289b95ae3425c03afc2c4
Samael Marjan <samaaelmarjan@gmail.com>
2022-09-18 23:24:26 +0600

    update-App-sep-18-23-24

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import './App.css';
+import React from 'react';
+
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Videos</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Box sx={{backgroundColor: '#000'}}>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+          <Route />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
